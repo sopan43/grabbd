285730f4c2cba4d913d9b13aec04b71128f205ca
Sopan <sopan.mittal@weaveability.com>
2018-10-28 13:11:56 +0530

    sql reconnect

--- connection.js
+++ connection.js
@@ -15,4 +15,20 @@ connection.connect(function(error) {
     console.log('Connecting.... ');
 });
 
+function handleDisconnect(client) {
+  client.on('error', function (error) {
+    if (!error.fatal) return;
+    if (error.code !== 'PROTOCOL_CONNECTION_LOST') throw err;
+
+    console.error('> Re-connecting lost MySQL connection: ' + error.stack);
+
+    // NOTE: This assignment is to a variable from an outer scope; this is extremely important
+    // If this said `client =` it wouldn't do what you want. The assignment here is implicitly changed
+    // to `global.mysqlClient =` in node.
+    mysqlClient = mysql.createConnection(client.config);
+    handleDisconnect(mysqlClient);
+    mysqlClient.connect();
+  });
+};
+
 module.exports = connection;
